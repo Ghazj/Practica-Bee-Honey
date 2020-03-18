6c3a3328eb4499219568a2501f1dee0ecb930586
diego29carlos <juan29-diego@hotmail.com>
2020-03-18 15:49:53 -0300

    topbar

--- bee-honey/src/App.js
+++ bee-honey/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+import Topbar from './components/Topbar/Topbar';
+import SideDrawer from './components/SideDrower/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
+
+
+  
+class App extends Component {
+  state = {
+   sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+     this.setState((prevState) => { 
+       return {sideDrawerOpen: !prevState.sideDrawerOpen};
+     });
+  };
+
+  backdropClickHandler = () => {
+     this.setState({sideDrawerOpen: false});
+  };
+
+  render() {
+    let backdrop;
+    
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop  click={this.backdropClickHandler} />
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: '100%'}}>
+     <Topbar  drawerClickHandler={this.drawerToggleClickHandler} />
+     <SideDrawer show={this.state.sideDrawerOpen} />
+    {backdrop}
+     <main style={{marginTop: '64px'}}>
+     <p>This is the page content!</p>
+     </main>
     </div>
-  );
+  ); 
+ }
 }
 
 export default App;
