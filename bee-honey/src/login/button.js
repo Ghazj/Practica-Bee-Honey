f60daae60bef01b141aab7925826a172a79740af
Ghazj <ghazijj98@gmail.com>
2020-03-20 14:42:46 -0300

    Se migra todo el desarrollo del login en de la rama login a esta rama login2, se soluciona un problema en el archivo button.js

---
+++ bee-honey/src/login/button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './login.css';
+
+function Button() {
+    return (
+        <div>
+          <button>Ingresar</button>
+        </div>
+    );
+}
+
+export default Button;
