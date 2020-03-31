4980557336b4dae97139eac74940d0524a300711
Ghazj <ghazijj98@gmail.com>
2020-03-31 14:04:31 -0300

    Hago organización de archivos para desarrollar la pantalla modal donde se podrá eliminar comentarios.

--- bee-honey/src/screens/detailScreen/detailScreen.js
+++ bee-honey/src/screens/detailScreen/detailScreen.js
@@ -3,16 +3,20 @@ import Peso from './components/peso/peso.js';
 import HumTemp from './components/humTemp/humTemp.js';
 import CantidadYPorcentaje from './components/cantidadYPorcentaje/cantidadYPorcentaje.js';
 import Notes from './components/notes/notes.js';
+import DeleteCommentModalScreen from './modals/deleteCommentModalScreen/deleteCommentModalScreen.js';
 
-function DetailScreen() {
-    return (
-        <>
-            <Peso />
-            <HumTemp />
-            <CantidadYPorcentaje/>
-            <Notes/>
-        </>
-    )
+class DetailScreen extends React.Component {
+    render() {
+        return (
+            <>
+                <Peso />
+                <HumTemp />
+                <CantidadYPorcentaje />
+                <Notes />
+                <DeleteCommentModalScreen/>
+            </>
+        )
+    }
 }
 
 export default DetailScreen;
---
+++ bee-honey/src/screens/detailScreen/modals/deleteCommentModalScreen/deleteCommentModalScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './deleteCommentModalScreen.css';
+
+class DeleteCommentModalScreen extends React.Component{
+    render(){
+        return(
+            <div>
+            <p>
+                Modal Delete Comment
+            </p>
+            </div>
+        )
+    }
+}
+
+export default DeleteCommentModalScreen;
